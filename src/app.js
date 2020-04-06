21535cb3f5ccae350102ffa456dccab21c6716ea
brokentime906 <brokentime906@gmail.com>
2020-04-06 13:40:19 +0900

    cors proablem

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var cors = require("cors");
 var indexRouter = require("./routes/index");
 
 var app = express();
@@ -13,11 +13,16 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+const corsOptions = {
+  origin: "https://todolist-web.netlify.com/",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
