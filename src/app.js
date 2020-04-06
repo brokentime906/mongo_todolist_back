de056d4d001ae0192ee6544922cc930cfd10b9d0
brokentime906 <brokentime906@gmail.com>
2020-04-06 13:49:40 +0900

    why,,, just one slash difference

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 const corsOptions = {
-  origin: "https://todolist-web.netlify.com/",
+  origin: "https://todolist-web.netlify.com",
   credentials: true,
 };
 
