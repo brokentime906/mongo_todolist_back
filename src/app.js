3fb7a045f41a2e0e71f5deec2b19d7068b9e2691
brokentime906 <brokentime906@gmail.com>
2020-04-09 14:50:53 +0900

    cors allow to every ip

--- src/app.js
+++ src/app.js
@@ -9,16 +9,7 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-const corsOptions = {
-  origin: "https://todolist-web.netlify.com",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
